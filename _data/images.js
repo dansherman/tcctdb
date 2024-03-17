8aad22c27a903cd6e81364ce9f809bfa771f0f09
dansherman <dan@dksherman.com>
2024-03-17 17:39:31 -0500

    Improving list layouts

--- _data/images.js
+++ _data/images.js
@@ -4,7 +4,7 @@ const {client} = require("../utils/sanity.js");
 
 async function getImages() {
   // Learn more: https://www.sanity.io/docs/data-store/how-queries-work
-  const query = groq`*[_type == 'photo']{_id,photo,caption}`;
+  const query = groq`*[_type == 'photo']{_id,photo,caption, "attribution":attribution->name,roles[]->{"characterName":character->characterName, castMember{"name":person->nameFirst + " " + person->nameLast, "slug":person->slug}}}`;
   const photos = await client.fetch(query).catch((err) => console.error(err));
   return photos;
 }
